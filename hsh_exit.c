57e5264eb057c2e1412dfa358503d8e7c0420409
ggirlk <ggirlk2@gmail.com>
2019-11-27 23:25:15 +0000

    exit with status

--- hsh_exit.c
+++ hsh_exit.c
@@ -1,11 +1,21 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
 /**
  * hsh_exit - builtin command: exit.
  * @a: list of args.  Not examined.
  * @p: the name of the executable program
  * Return: Always returns 0, to terminate execution.
  */
-int hsh_exit(char **a __attribute__((unused)), char *p __attribute__((unused)))
+int hsh_exit(char **a, char *p __attribute__((unused)))
 {
-exit(110);
+int status;
+if (a[1])
+{
+status = _atoi(a[1]);
+exit(status);
+}
+else
+exit(0);
+return (1);
 }
